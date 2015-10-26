1fc8897041c1fa0f6a48c835fd54adf2a46d783c
TJ Draper <tj@caddis.co>
2015-10-26 10:45:17 -0500

    Add support for ExpressionEngine 3

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Streeng 1.6.0
+# Streeng 1.7.0
 
 Perform common operations on strings such as change case, truncate, find/replace, repeat, encode/decode, generate slugs and more.
 
--- system/expressionengine/third_party/streeng/pi.streeng.php
+++ system/expressionengine/third_party/streeng/pi.streeng.php
@@ -1,11 +1,13 @@
 <?php if (! defined('BASEPATH')) exit('No direct script access allowed');
 
+include_once(PATH_THIRD . 'streeng/addon.setup.php');
+
 $plugin_info = array (
-	'pi_name' => 'Streeng',
-	'pi_version' => '1.6.0',
-	'pi_author' => 'Caddis',
-	'pi_author_url' => 'http://www.caddis.co',
-	'pi_description' => 'Perform common operations on strings.',
+	'pi_author' => STREENG_AUTHOR,
+	'pi_author_url' => STREENG_AUTHOR_URL,
+	'pi_description' => STREENG_DESC,
+	'pi_name' => STREENG_NAME,
+	'pi_version' => STREENG_VER,
 	'pi_usage' => Streeng::usage()
 );
 
@@ -361,49 +363,7 @@ class Streeng {
 
 	public static function usage()
 	{
-		ob_start();
-?>
-Parameters:
-
-allowed="p|span|a" - pass "none" to strip all tags or a | delimited list of allowed tags (defaults = allow all)
-find="string1" - string or to find (default = false, separate multiple values with a the explode variable which defaults to "|", accepts regex)
-replace="string2" - string or to replace found string (default = "")
-explode="|" - string to split find/replace parameters with
-trim="left" - left, right, or both (default = "both")
-encode="yes" - HTML encode the string (default = "no")
-decode="yes" - HTML decode the string (default = "no")
-url="yes" - URL encode the string (default = "no")
-capitalize="yes" - capitalize the first character of the string (default = "no")
-title="yes" - capitalize the first character of every word (default = "no")
-lower="yes" - convert the string to lower case (default = "no")
-upper="yes" - convert the string to upper case (default = "no")
-characters="10" - number of characters to truncate the string to (default = unlimited)
-words="10" - number of words to truncate the string to (default = unlimited)
-append="..." - if trucated append this to the end of the string (default = "&hellip;")
-slug="yes" - convert the string to a slug (default = "no")
-separator="_" - seperator for slug (default = "-")
-repeat="3" - number of times to repeat the string, great for prototyping (default = 0)
-insensitive="yes" - toggle case sensitivity when finding a string (default = "no")
-count="|" - return number of substring instances of a supplied string (default = false)
-splits="|" - return number of exploded values from a supplied string (default = false)
-
-Usage:
-
-{exp:streeng allowed="<p>" title="yes" repeat="2" find=" a " replace=" my "}<p><b>This</b> is a <a href="#">test</a>.</p>{/exp:streeng}
-
-{if "{exp:streeng find='this' insensitive='yes'}This is a test string{/exp:streeng}"}
-We found 'this' in 'This is a test string'!
-{/if}
-
-<p>This Is My Test</p>
-<p>This Is My Test</p>
-<p>This Is My Test</p>
-<?php
-		$buffer = ob_get_contents();
-
-		ob_end_clean();
-
-		return $buffer;
+		return 'See docs and examples at https://github.com/caddis/streeng';
 	}
 }
 ?>
